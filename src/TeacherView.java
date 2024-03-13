33f8dc83170fe84a9f5c4776edfe868d4966c4e9
lena0585 <125387757+lena0585@users.noreply.github.com>
2024-03-13 18:56:29 +0300

    Update TeacherView.java

--- src/TeacherView.java
+++ src/TeacherView.java
@@ -1,5 +1,5 @@
 import java.util.List;
-
+//Принцип единой ответственности
 public class TeacherView implements UserView<Teacher>{
     @Override
     public void sendOnConsole(List<Teacher> list) {
